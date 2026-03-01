f33de477f0a1b86034da48365520a38177404ece
abdelra7man <abdelra7manabdullah@gmail.com>
2026-03-01 23:51:52 +0200

    FEAT(provider): Adding Rate Limitting For In AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,33 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureRateLimiting();
+    }
+
+    private function configureRateLimiting(): void
+    {
+        // General API limit — 60 requests per minute per user/IP
+       RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+        // authentication routes — 10 requests per minute per IP
+        RateLimiter::for('auth', function (Request $request) {
+            return Limit::perMinute(10)
+            ->by($request->ip())
+            ->response(function () {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Too many login attempts. Please try again later.',
+                    'error'   => null
+                ], 429);
+            });
+        });
+
+        // Location updates — driver sends every 5 seconds
+        RateLimiter::for('location', function (Request $request) {
+            return Limit::perMinute(30)->by($request->user()?->id ?: $request->ip());
+        });
+        
     }
 }
